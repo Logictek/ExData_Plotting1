6ab47d439dde39420a7539992e6322a57b04dcf1
Logictek <googlex2@Alan-Lytzs-MacBook-Pro.local>
2015-11-08 18:14:21 -0500

    final plots

---
+++ plot1.R
@@ -0,0 +1,8 @@
+data<- read.table("household_power_consumption.txt", sep=";", header=T, quote= "", strip.white=TRUE, stringsAsFactors = F, na.strings= "?")
+
+# Subsetting the full data to obtain the data related to two days: 
+data<- subset(data, (data$Date == "1/2/2007" | data$Date== "2/2/2007")) 
+
+# creating Plot1
+
+hist(data$Global_active_power, col= "red", xlab= "Global Active Power (kilowatts)", ylab= "Frequency", main= "Global Active Power")
---
+++ plot2.R
@@ -0,0 +1,10 @@
+data<- read.table("household_power_consumption.txt", sep=";", header=T, quote= "", strip.white=TRUE, stringsAsFactors = F, na.strings= "?")
+
+# Subsetting the full data to obtain the data related to two days: 
+data<- subset(data, (data$Date == "1/2/2007" | data$Date== "2/2/2007")) 
+# change the date format
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+
+data$DateTime <- as.POSIXct(paste(data$Date, data$Time))
+
+plot(data$DateTime, data$Global_active_power, type= "l", lwd=1, ylab= "Global Active Power (kilowatts)", xlab="")
